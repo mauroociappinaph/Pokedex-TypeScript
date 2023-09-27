d9b3faf7adae1803f2659dc25f8985561a755ce6
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-09-27 13:04:00 -0300

    Configuración de Rutas

--- Pokedex/src/App.tsx
+++ Pokedex/src/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
+
+
 
 import './App.css';
+import { Items, Pokemon, Pokemons } from "./pages";
 
 function App() {
   return (
+    <Router>
     <div className="app">
-      <h1>Pokedex</h1>
+      <Routes>
+        <Route path="/" element={< Pokemons />} />
+        <Route path="/items" element={< Items />} />
+        <Route path="/pokemons" element={< Pokemons />} />
+        <Route path="/pokemons/:name" element={< Pokemon />} />
+  
+      </Routes>
     </div>
+    </Router>
   );
 }
 
---
+++ Pokedex/src/pages/index.tsx
@@ -0,0 +1,3 @@
+export {default as Pokemons} from './Pokemons';
+export {default as Items} from './items';
+export {default as Pokemon} from './Pokemon';

9f21ad4df583fc413e4ea80ed07572b6080a8bdc
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-09-30 17:46:08 -0300

    Main

--- Pokedex/src/pages/Pokemons.tsx
+++ Pokedex/src/pages/Pokemons.tsx
@@ -1,21 +1,34 @@
-import  Headers  from '../components/Headers';
-import  Main  from '../components/Main'
-import  Footer  from '../components/Footer';
-import  { useState } from "react";
+import Headers from '../components/Headers';
+import Main from '../components/Main';
+import Footer from '../components/Footer';
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import styles from "./pokemons.module.css"; // Importa los estilos CSS modules
+
+import BulbasourPic from "../assets/bulbasaur.gif";
 
 const Pokemons = () => {
-    const [query , setQuery] = useState ("");
-    return (
-        <>
-        <Headers query={query} setQuery={setQuery} />
+  const [query, setQuery] = useState("");
 
-        <Main />
+  return (
+    <div className={styles.app}>
+      <Headers query={query} setQuery={setQuery} />
 
-        <Footer />
+      
+        <nav className={styles.main}>
+          <Link to="/" className={styles.link}>
+            <img src={BulbasourPic} alt="Bulbasour" />
+            <div className={styles.container}>
+              <span className={styles.linkText}>Bulbasour</span>
+              <span>001</span>
+            </div>
+          </Link>
+        </nav>
+      
 
-        
-        </>
-    );
-}
+      <Footer />
+    </div>
+  );
+};
 
-export default Pokemons 
+export default Pokemons;

4c4502fefe43b8d3ba37c0ed51ef884d2c5d6b33
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-10-02 11:42:01 -0300

    Set Pokemon y useEffect

--- Pokedex/src/pages/Pokemons.tsx
+++ Pokedex/src/pages/Pokemons.tsx
@@ -2,13 +2,22 @@ import Headers from '../components/Headers';
 import Main from '../components/Main';
 import Footer from '../components/Footer';
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState , useEffect } from "react";
 import styles from "./pokemons.module.css"; // Importa los estilos CSS modules
-
+import { fetchPokemons } from '../api/fetchPokemons';
 import BulbasourPic from "../assets/bulbasaur.gif";
 
 const Pokemons = () => {
   const [query, setQuery] = useState("");
+  const [pokemons , setPokemons] = useState([]);
+
+  useEffect(() => {
+    const fetchAllPokemons = async () => {
+    const allPokemons = await fetchPokemons();
+    setPokemons(allPokemons);
+  }
+  fetchAllPokemons();
+  }, [])
 
   return (
     <div className={styles.app}>

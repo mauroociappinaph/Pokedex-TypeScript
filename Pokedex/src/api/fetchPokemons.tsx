eccd75700411b9bbf4511398f3d7bb81ddb6e803
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-10-03 11:53:29 -0300

    Fetch aun no funcionando

--- Pokedex/src/api/fetchPokemons.tsx
+++ Pokedex/src/api/fetchPokemons.tsx
@@ -1,23 +1,41 @@
-// const url = "https://unpkg.com/pokemons@1.1.0/pokemons.json"
-import { formatPokemonName } from "../utils/utils"
-export async function fetchPokemons(){
-    const response = await fetch("https://unpkg.com/pokemons@1.1.0/pokemons.json");
-   
-    if (!response.ok) {
-      throw new Error(response.statusText);
+import { formatPokemonName } from "../utils/utils";
+
+interface PokemonData {
+  name: string;
+  id: number;
+}
+
+interface Pokemon {
+  name: string;
+  id: number;
+  image: string;
+}
+
+export async function fetchPokemons(): Promise<Pokemon[]> {
+  const response = await fetch("https://unpkg.com/pokemons@1.1.0/pokemons.json");
+
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+
+  const data = await response.json();
+  const results: PokemonData[] = data.results;
+
+  const pokemons: Pokemon[] = results.map((pokemon: PokemonData) => {
+    console.log(`Creating Pokemon: ${pokemon.name}`); // Added console.log statement
+    return {
+      name: pokemon.name,
+      id: pokemon.id,
+      image: `https://img.pokemondb.net/sprites/black-white/anim/normal/${formatPokemonName(pokemon.name)}.gif`,
+    };
+  });
+
+  const uniquePokemons = pokemons.filter(
+    (pokemon: Pokemon, index: number) => {
+      console.log(`Filtering Pokemon: ${pokemon.name}`); // Added console.log statement
+      return pokemons.findIndex((other: Pokemon) => other.id === pokemon.id) === index;
     }
-    const results = await response.json();
-    console.log(results);
-    
-    const pokemons = results.map((pokemon : any) => {
-        return {
-            name: pokemon.name,
-            id: pokemon.id,
-            image: `https://img.pokemondb.net/sprites/black-white/anim/normal/${formatPokemonName(pokemon.name)}.gif`,
-        };
-    });
-    const uniquePokemons = pokemons.filter(
-        (pokemon: any , index: number) => pokemons.findIndex((other: any) => other.id === pokemon.id) === index
-    )
-    return uniquePokemons;
+  );
+
+  return uniquePokemons;
 }
--- Pokedex/src/pages/Pokemons.tsx
+++ Pokedex/src/pages/Pokemons.tsx
@@ -1,39 +1,48 @@
-import Headers from '../components/Headers';
-import Main from '../components/Main';
-import Footer from '../components/Footer';
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { useState , useEffect } from "react";
+import Headers from "../components/Headers";
+import Footer from "../components/Footer";
 import styles from "./pokemons.module.css"; // Importa los estilos CSS modules
 import { fetchPokemons } from '../api/fetchPokemons';
-import BulbasourPic from "../assets/bulbasaur.gif";
+
+interface Pokemon {
+  id: number;
+  name: string;
+  image: string;
+}
 
 const Pokemons = () => {
   const [query, setQuery] = useState("");
-  const [pokemons , setPokemons] = useState([]);
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
 
   useEffect(() => {
     const fetchAllPokemons = async () => {
-    const allPokemons = await fetchPokemons();
-    setPokemons(allPokemons);
-  }
-  fetchAllPokemons();
-  }, [])
+      try {
+        const allPokemons = await fetchPokemons();
+        setPokemons(allPokemons);
+      } catch (error) {
+        console.error("Error fetching Pokemon data:", error);
+      }
+    };
+    fetchAllPokemons();
+  }, []);
 
   return (
     <div className={styles.app}>
       <Headers query={query} setQuery={setQuery} />
 
-      
-        <nav className={styles.main}>
-          <Link to="/" className={styles.link}>
-            <img src={BulbasourPic} alt="Bulbasour" />
+      <nav className={styles.main}>
+        {pokemons.slice(0, 152).map((pokemon) => (
+          <Link to={`/pokemons/${pokemon.name.toLowerCase()}`} 
+          className={styles.link} key={pokemon.id}>
+            <img src={pokemon.image} alt={pokemon.name} />
             <div className={styles.container}>
-              <span className={styles.linkText}>Bulbasour</span>
-              <span>001</span>
+              <span className={styles.linkText}>{pokemon.name}</span>
+              <span>{pokemon.id}</span>
             </div>
           </Link>
-        </nav>
-      
+        ))}
+      </nav>
 
       <Footer />
     </div>
